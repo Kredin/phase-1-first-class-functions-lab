d8848d4f7d88e568e1db20405276cf4dad69d615
Blade <bladeprojectz@gmail.com>
2024-02-02 19:12:45 +0800

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(value){
+    return [value[0], value[1]]
+}
+
+const returnLastTwoDrivers = function(value){
+    return [value[2], value[3]]
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(value){
+    return function(fare){
+        return fare * value
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(drivers, func){
+    if(func === returnFirstTwoDrivers){
+        return returnFirstTwoDrivers(drivers)
+    }else if(func === returnLastTwoDrivers){
+        return returnLastTwoDrivers(drivers)
+    }
+}
